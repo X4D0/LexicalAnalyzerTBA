afbf3ca66b8d42c9b40cf3048d3fcda426a50357
Ray Rahmanda <46711970+X4D0@users.noreply.github.com>
2019-11-19 17:56:00 +0700

    Bismillah
    "...))" Still Error

--- TBA/src/LexicalAnalyzer.java
+++ TBA/src/LexicalAnalyzer.java
@@ -5,198 +5,8 @@ import java.util.*;
  */
 public class LexicalAnalyzer {
     public static void main(String[] args) {
-//        Stack stack = new Stack(); /* Make a New Stack For Characters*/
-//        
-//        Scanner input = new Scanner(System.in);
-//        System.out.print("Input : ");
-//        String s = input.nextLine(); /* Input String */
-//        char[] arrayChar = s.toCharArray(); /* Every char in String s will be added to Array */
-//        
-//        /* Push every Char in arrayChar into Stack */
-//        for (int i = arrayChar.length - 1; i >= 0; i--) {
-//            stack.add(arrayChar[i]);
-//        }
-//        
-//        System.out.print("\nToken : ");
-//        /* Checking Process */
-//        while(!stack.isEmpty()){
-//            /* Token 1 (p,q,r,s) */
-//            char pop = (char) stack.pop();
-//            if (pop == 'p' || pop == 'q' || pop == 'r' || pop == 's'){
-//                System.out.print(" 1 |");
-//                
-//            }
-//            
-//            /* Token 2 (not) */
-//            if(pop == 'n'){
-//                pop = (char) stack.pop();
-//                if(pop == 'o'){
-//                    pop = pop = (char) stack.pop();;
-//                    if(pop == 't'){
-//                        System.out.print(" 2 |");
-//                        
-//                    }
-//                }
-//            }
-//            
-//            /* Token 3 (and) */
-//            if(pop =='a'){
-//                pop = (char) stack.pop();
-//                if(pop == 'n'){
-//                    pop = (char) stack.pop();
-//                    if(pop == 'd'){
-//                        System.out.print(" 3 |");
-//                    }
-//                }
-//            }
-//            
-//            /* Token 4 (or) */
-//            if(pop == 'o'){
-//                pop = (char) stack.pop();
-//                if(pop == 'r'){
-//                    System.out.print(" 4 |");
-//                }
-//            }
-//            
-//            /* Token 5 (xor) */
-//            if(pop == 'x'){
-//                pop = (char) stack.pop();
-//                if(pop == 'o'){
-//                    pop = (char) stack.pop();
-//                    if(pop == 'r'){
-//                        System.out.print(" 5 |");
-//                    }
-//                }
-//            }
-//            
-//            /* Token 6 (if) */
-//            if(pop == 'i'){
-//                pop = (char) stack.pop();
-//                if(pop == 'f'){
-//                    System.out.print(" 6 |");
-//                }
-//            }
-//            
-//            /* Token 7 (then) */
-//            if(pop == 't'){
-//                pop = (char) stack.pop();
-//                if(pop == 'h'){
-//                    pop = (char) stack.pop();
-//                    if(pop == 'e'){
-//                        pop = (char) stack.pop();
-//                        if(pop == 'n'){
-//                            System.out.print(" 7 |");
-//                        }
-//                    }
-//                }
-//            }
-//            
-//            /* Token 8 (iff) */
-//            if(pop == 'i'){
-//                pop = (char) stack.pop();
-//                if(pop == 'f'){
-//                    pop = (char) stack.pop();
-//                    if(pop == 'f'){
-//                        System.out.print(" 8 |");
-//                    }else{
-//                        System.out.print("ERROR |");
-//                    }
-//                }
-//            }
-//            
-//            /* Token 9 ( '(' ) */
-//            if(pop == '('){
-//                System.out.print(" 9 |");
-//                while(pop != ')'){
-//                    pop = (char) stack.pop();
-//                    if (pop == 'p' || pop == 'q' || pop == 'r' || pop == 's'){
-//                    System.out.print(" 1 |");
-//                    }
-//
-//                    /* Token 2 (not) */
-//                    if(pop == 'n'){
-//                        pop = (char) stack.pop();
-//                        if(pop == 'o'){
-//                            pop = (char) stack.pop();
-//                            if(pop == 't'){
-//                                System.out.print(" 2 |");
-//                            }
-//                        }
-//                    }
-//
-//                    /* Token 3 (and) */
-//                    if(pop == 'a'){
-//                        pop = (char) stack.pop();
-//                        if(pop == 'n'){
-//                            pop = (char) stack.pop();
-//                            if(pop == 'd'){
-//                                System.out.print(" 3 |");
-//                            }
-//                        }
-//                    }
-//
-//                    /* Token 4 (or) */
-//                    if(pop == 'o'){
-//                        pop = (char) stack.pop();
-//                        if(pop == 'r'){
-//                            System.out.print(" 4 |");
-//                        }
-//                    }
-//
-//                    /* Token 5 (xor) */
-//                    if(pop == 'x'){
-//                        pop = (char) stack.pop();
-//                        if(pop == 'o'){
-//                            pop = (char) stack.pop();
-//                            if(pop == 'r'){
-//                                System.out.print(" 5 |");
-//                            }
-//                        }
-//                    }
-//
-//                    /* Token 6 (if) */
-//                    if(pop == 'i'){
-//                        pop = (char) stack.pop();
-//                        if(pop == 'f'){
-//                            System.out.print(" 6 |");
-//                        }
-//                    }
-//
-//                    /* Token 7 (then) */
-//                    if(pop == 't'){
-//                        pop = (char) stack.pop();
-//                        if(pop == 'h'){
-//                            pop = (char) stack.pop();
-//                            if(pop == 'e'){
-//                                pop = (char) stack.pop();
-//                                if(pop == 'n'){
-//                                    System.out.print(" 7 |");
-//                                }
-//                            }
-//                        }
-//                    }
-//
-//                    /* Token 8 (iff) */
-//                    if(pop == 'i'){
-//                        pop = (char) stack.pop();
-//                        if(pop == 'f'){
-//                            pop = (char) stack.pop();
-//                            if(pop == 'f'){
-//                                System.out.print(" 8 |");
-//                            }
-//                        }
-//                    }
-//                    
-//                    if(pop == ')'){
-//                        System.out.print(" 10 |");
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
-
-int i = 0;
+        int i = 0;
+        int temp = 0;
         
         Stack stack = new Stack(); /* Make a New Stack For Characters*/
         Stack validasi = new Stack(); /* Make another Stack for Validation */
@@ -222,7 +32,7 @@ int i = 0;
             }
 
             /* Token 2 (not) */
-            else if(arrayChar[i] == 'n' && arrayChar[i+1] == 'o' && arrayChar[i+2] == 't' && ((char)stack.peek() != 'p' && (char)stack.peek() != 'q' && (char)stack.peek() != 'r' && (char)stack.peek() != 's'))
+            else if(arrayChar[i] == 'n' && arrayChar[i+1] == 'o' && arrayChar[i+2] == 't')
             {
                 stack.push(arrayChar[i]);
                 stack.push(arrayChar[i+1]);
@@ -265,7 +75,7 @@ int i = 0;
             }
             
             /* Token 6 (if) */
-            else if(arrayChar[i] == 'i' && arrayChar[i+1] == 'f' && (arrayChar[i+2] == ' ')) // ||arrayChar[i+2] == 'p' || arrayChar[i+2] == 'q' || arrayChar[i+2] == 'r' || arrayChar[i+2] == 's' || arrayChar[i+2] == '(') && ((char)stack.peek() != 'p' && (char)stack.peek() != 'q' && (char)stack.peek() != 'r' && (char)stack.peek() != 's'))
+            else if(arrayChar[i] == 'i' && arrayChar[i+1] == 'f' && (arrayChar[i+2] == ' ' ||arrayChar[i+2] == 'p' || arrayChar[i+2] == 'q' || arrayChar[i+2] == 'r' || arrayChar[i+2] == 's' || arrayChar[i+2] == '('))
             {
                 stack.push(arrayChar[i]);
                 stack.push(arrayChar[i+1]);
@@ -334,17 +144,30 @@ int i = 0;
             System.out.print("\nOutput : ");
             while(!validasi.empty())
             {
-                validasi.pop();
-                int temp = (int) validasi.peek();
-//                if((int) validasi.peek() == 999)
-//                {
-//                    System.out.print("NOT VALID");
-//                }
-                if (validasi.empty()) {
-                    System.out.print("VALID");
-                }
-                if ((int) validasi.peek() != 10 && temp == (int)validasi.peek()){
-                    System.out.print("NOT VALID");
+                try{
+                    if(validasi.peek().equals(999))
+                    {
+                        System.out.print("NOT VALID");
+                        break;
+                    }
+
+                    temp = (int)validasi.peek();
+                    validasi.pop();
+
+                    if (validasi.empty()) {
+                        System.out.print("VALID");
+                        break;
+                    }
+
+                    if(validasi.peek().equals(temp) && validasi.peek().equals(10)){
+                        System.out.print("NOT VALID");
+                        break;
+                    }else if (temp == 2 || temp == 6 || temp == 9 && validasi.peek().equals(1)){
+                        System.out.print("NOT VALID");
+                        break;
+                    }
+                }catch(EmptyStackException e){
+                    //Nothing to do
                 }
             }
     }
